f88f076f9d0efed368c57afd39a4110851e1a8c4
darshik001 <darshikshekhada07@gmail.com>
2025-12-24 11:25:33 +0530

    first

---
+++ Pr-1/app.js
@@ -0,0 +1,36 @@
+const http = require('http')
+const fs =  require('fs')
+
+const port = 8000
+const server  = http.createServer((req,res)=>{
+ let filepath;
+  switch (req.url) {
+    case '/':
+        filepath = './Home.html'
+        break;
+    case '/services':
+        filepath = './services.html'
+        break;    
+     case '/about':    
+        filepath = './about.html'
+        break;
+     case '/blog':
+        filepath = './blog.html'
+        break;
+      case '/contact':
+        filepath = './contact.html'
+        break;
+
+    default:
+        filepath = './NoFound.html'
+        break;
+  }
+     let data = fs.readFileSync(filepath,'utf-8')
+     res.write(data)
+     res.end()
+})
+
+server.listen(port,(err)=>{
+  if(err) console.log(err)
+    else console.log(`http://localhost:${port}`)
+})
