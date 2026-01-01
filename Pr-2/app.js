31fb424a2ba7b4ec1e4818613c693879da75ccd7
darshik001 <darshikshekhada07@gmail.com>
2026-01-01 11:26:55 +0530

    done

--- Pr-2/app.js
+++ Pr-2/app.js
@@ -1,117 +1,87 @@
 const express = require('express')
+const port = 8000
 const app = express()
-const port = 8080
-
-app.set('view engine','ejs')
-// app.use(express.urlencoded())
+app.set("view engine","ejs")
 app.use(express.static('public'))
+app.use(express.urlencoded())
+
+let todos = [
+    {
+        id:"1",
+  title: 'meeting',
+  desc: 'meeting with client',
+  priority: 'low',
+  category: 'work',
+  status: 'pending',
+  date: '2025-12-31'
+},
+    {
+  id:"2",
+  title: 'meeting',
+  desc: 'meeting with client',
+  priority: 'low',
+  category: 'work',
+  status: 'pending',
+  date: '2025-12-31'
+}
+
+]
+
+
 app.get('/',(req,res)=>{
-   res.render("index")
+    return res.render('index',{todos})
 })
 
-app.get('/charts',(req,res)=>{
-   res.render("charts")
-
+app.get('/add-task',(req,res)=>{
+    return res.render("add")
 })
 
+app.post('/add-todo',(req,res)=>{
 
-app.get('/widgets',(req,res)=>{
-   res.render("widgets")
 
-})
-
-app.get('/tables',(req,res)=>{
-   res.render("tables")
-
-})
-
-app.get('/grid',(req,res)=>{
-   res.render("grid")
-
-})
-
-app.get('/form-basic',(req,res)=>{
-   res.render("form-basic")
-
-})
-app.get('/form-wizard',(req,res)=>{
-   res.render("form-wizard")
-
-})
-
-app.get('/pages-buttons',(req,res)=>{
-   res.render("pages-buttons")
-
-})
-
-app.get('/icon-material',(req,res)=>{
-   res.render("icon-material")
-
-})
-app.get('/icon-fontawesome',(req,res)=>{
-   res.render("icon-fontawesome")
-
-})
-
-app.get('/pages-elements',(req,res)=>{
-   res.render("pages-elements")
-
-})
-
-app.get('/index2',(req,res)=>{
-   res.render("index2")
-
-})
-app.get('/pages-gallery',(req,res)=>{
-   res.render("pages-gallery")
-
-})
-
-app.get('/pages-calendar',(req,res)=>{
-   res.render("pages-calendar")
-
-})
-
-app.get('/pages-invoice',(req,res)=>{
-   res.render("pages-invoice")
-
-})
-
-app.get('/pages-chat',(req,res)=>{
-   res.render("pages-chat")
-
+   todos.push({
+    ...req.body,
+    id:(todos.length+1).toString()
+   })
+   res.redirect('/')
 })
 
 
-app.get('/authentication-login',(req,res)=>{
-   res.render("authentication-login")
-
-})
-app.get('/authentication-register',(req,res)=>{
-   res.render("authentication-register")
-
+app.get('/delete-todo/:id',(req,res)=>{
+let id = req.params.id
+todos =  todos.filter((todo)=> todo.id !==id);
+return res.redirect('/')
 })
 
-app.get('/error-403',(req,res)=>{
-   res.render("error-403")
 
+app.get('/edit-todo/:id',(req,res)=>{
+let id = req.params.id
+let todo =  todos.find((todo)=> todo.id ==id);
+return res.render('edittodo',{todo})
 })
 
-app.get('/error-404',(req,res)=>{
-   res.render("error-404")
 
+app.post('/update-todo/:id',(req,res)=>{
+let id = req.params.id
+ let update =  todos.map((todo)=>{
+        if(todo.id == id){
+            return{
+                ...req.body,
+                id:id,
+            }
+        }else{
+            return todo
+        }
+    })
+
+    todos = update
+
+    res.redirect('/')
 })
 
-app.get('/error-405',(req,res)=>{
-   res.render("error-405")
 
-})
-app.get('/error-500',(req,res)=>{
-   res.render("error-500")
 
-})
- 
+
 app.listen(port,()=>{
     console.log(`Server start at http://localhost:${port}`)
-
 })
